cb010a11dd94b129b53629d1c47d7ae880c1ea6f
Stefan <Huth@Stefans-MacBook-Pro-2.local>
2016-09-26 17:38:58 +0100

    creepy crawly initial commit

---
+++ src/main/java/com/dotsh/creepycrawlies/CreepyCrawlyApplication.java
@@ -0,0 +1,13 @@
+package com.dotsh.creepycrawlies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreepyCrawlyApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(CreepyCrawlyApplication.class, args);
+    }
+
+}
