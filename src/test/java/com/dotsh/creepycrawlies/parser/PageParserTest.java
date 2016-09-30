e574bf1d8d7e7e242a9cb0ec22f65e14a3367bb8
Stefan <Huth@Stefans-MacBook-Pro-2.local>
2016-09-30 18:30:33 +0100

    refactoring page parser tests

--- src/test/java/com/dotsh/creepycrawlies/parser/PageParserTest.java
+++ src/test/java/com/dotsh/creepycrawlies/parser/PageParserTest.java
@@ -1,6 +1,5 @@
 package com.dotsh.creepycrawlies.parser;
 
-import com.dotsh.creepycrawlies.crawler.Crawler;
 import com.dotsh.creepycrawlies.model.Page;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
@@ -9,7 +8,6 @@ import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.List;
 
 import static com.dotsh.creepycrawlies.parser.PageParser.HREF_ATTRIBUTE;
 import static org.junit.Assert.assertEquals;
@@ -156,135 +154,95 @@ public class PageParserTest {
 
     @Test
     public void doesNotAddStaticContentIfNull() throws IOException {
-        class TestCrawler extends Crawler {
-            @Override
-            protected Document retrieveDocument(String url) {
-                Document document = mock(Document.class);
-                Element topLevelElement = mock(Element.class);
-                Element navElement = mock(Element.class);
-                Elements elements = new Elements();
-                Attributes attributes = mock(Attributes.class);
-                elements.add(0, navElement);
+        Document document = mock(Document.class);
+        Element topLevelElement = mock(Element.class);
+        Element navElement = mock(Element.class);
+        Elements elements = new Elements();
+        Attributes attributes = mock(Attributes.class);
+        elements.add(0, navElement);
 
-                when(document.body()).thenReturn(topLevelElement);
-                when(topLevelElement.select(anyString())).thenReturn(elements);
-                when(attributes.get("src")).thenReturn(null);
-                when(navElement.attributes()).thenReturn(attributes);
+        when(document.body()).thenReturn(topLevelElement);
+        when(topLevelElement.select(anyString())).thenReturn(elements);
+        when(attributes.get("src")).thenReturn(null);
+        when(navElement.attributes()).thenReturn(attributes);
 
-                return document;
-            }
-        }
-        TestCrawler crawler = new TestCrawler();
-        List<Page> pages = crawler.connect(WIPRO_HOMEPAGE);
-        assertEquals(0, pages.get(0).getStaticContent().size());
+        assertEquals(0, new PageParser().buildFromDocument(document, HOST_URL).getStaticContent().size());
     }
 
     @Test
     public void doesNotAddStaticContentIfEmpty() throws IOException {
-        class TestCrawler extends Crawler {
-            @Override
-            protected Document retrieveDocument(String url) {
-                Document document = mock(Document.class);
-                Element topLevelElement = mock(Element.class);
-                Element navElement = mock(Element.class);
-                Elements elements = new Elements();
-                Attributes attributes = mock(Attributes.class);
-                elements.add(0, navElement);
+        Document document = mock(Document.class);
+        Element topLevelElement = mock(Element.class);
+        Element navElement = mock(Element.class);
+        Elements elements = new Elements();
+        Attributes attributes = mock(Attributes.class);
+        elements.add(0, navElement);
 
-                when(document.body()).thenReturn(topLevelElement);
-                when(topLevelElement.select(anyString())).thenReturn(elements);
-                when(attributes.get("src")).thenReturn("");
-                when(navElement.attributes()).thenReturn(attributes);
+        when(document.body()).thenReturn(topLevelElement);
+        when(topLevelElement.select(anyString())).thenReturn(elements);
+        when(attributes.get("src")).thenReturn("");
+        when(navElement.attributes()).thenReturn(attributes);
 
-                return document;
-            }
-        }
-        TestCrawler crawler = new TestCrawler();
-        List<Page> pages = crawler.connect(WIPRO_HOMEPAGE);
-        assertEquals(0, pages.get(0).getStaticContent().size());
+        assertEquals(0, new PageParser().buildFromDocument(document, HOST_URL).getStaticContent().size());
     }
 
     @Test
     public void doesNotAttemptToAddUrlsToPageIfAttributesIsNull() throws IOException {
-        class TestCrawler extends Crawler {
-            @Override
-            protected Document retrieveDocument(String url) {
-                Document document = mock(Document.class);
-                Element topLevelElement = mock(Element.class);
-                Element navElement = mock(Element.class);
-                Elements elements = new Elements();
-                elements.add(0, navElement);
+        Document document = mock(Document.class);
+        Element topLevelElement = mock(Element.class);
+        Element navElement = mock(Element.class);
+        Elements elements = new Elements();
+        elements.add(0, navElement);
 
-                when(document.body()).thenReturn(topLevelElement);
-                when(topLevelElement.select(anyString())).thenReturn(elements);
-                when(navElement.attributes()).thenReturn(null);
+        when(document.body()).thenReturn(topLevelElement);
+        when(topLevelElement.select(anyString())).thenReturn(elements);
+        when(navElement.attributes()).thenReturn(null);
 
-                return document;
-            }
-        }
-        TestCrawler crawler = new TestCrawler();
-        List<Page> pages = crawler.connect(WIPRO_HOMEPAGE);
-        assertEquals(0, pages.get(0).getInternalUrls().size());
+        assertEquals(0, new PageParser().buildFromDocument(document, HOST_URL).getInternalUrls().size());
     }
 
     @Test
     public void doesNotAddQueryStringedUrlsTwice() throws IOException {
-        class TestCrawler extends Crawler {
-            @Override
-            protected Document retrieveDocument(String url) {
-                Document document = mock(Document.class);
-                Element topLevelElement = mock(Element.class);
-                Element navElement = mock(Element.class);
-                Element navElement2 = mock(Element.class);
-                Elements elements = new Elements();
-                Attributes attributes = mock(Attributes.class);
-                Attributes attributes2 = mock(Attributes.class);
-                elements.add(0, navElement);
-                elements.add(1, navElement2);
+        Document document = mock(Document.class);
+        Element topLevelElement = mock(Element.class);
+        Element navElement = mock(Element.class);
+        Element navElement2 = mock(Element.class);
+        Elements elements = new Elements();
+        Attributes attributes = mock(Attributes.class);
+        Attributes attributes2 = mock(Attributes.class);
+        elements.add(0, navElement);
+        elements.add(1, navElement2);
 
-                when(document.body()).thenReturn(topLevelElement);
-                when(topLevelElement.select(anyString())).thenReturn(elements);
-                when(attributes.get("href")).thenReturn("http://wiprodigital.com/what-we-do#work-three-circles-row");
-                when(attributes2.get("href")).thenReturn("http://wiprodigital.com/what-we-do");
-                when(navElement.attributes()).thenReturn(attributes);
-                when(navElement2.attributes()).thenReturn(attributes2);
+        when(document.body()).thenReturn(topLevelElement);
+        when(topLevelElement.select(anyString())).thenReturn(elements);
+        when(attributes.get("href")).thenReturn("http://wiprodigital.com/what-we-do#work-three-circles-row");
+        when(attributes2.get("href")).thenReturn("http://wiprodigital.com/what-we-do");
+        when(navElement.attributes()).thenReturn(attributes);
+        when(navElement2.attributes()).thenReturn(attributes2);
 
-                return document;
-            }
-        }
-        TestCrawler crawler = new TestCrawler();
-        List<Page> pages = crawler.connect(WIPRO_HOMEPAGE);
-        assertEquals(1, pages.get(0).getInternalUrls().size());
+        assertEquals(1, new PageParser().buildFromDocument(document, HOST_URL).getInternalUrls().size());
     }
 
     @Test
     public void doesNotAddQueryStringedUrlsWithQuestionMarkTwice() throws IOException {
-        class TestCrawler extends Crawler {
-            @Override
-            protected Document retrieveDocument(String url) {
-                Document document = mock(Document.class);
-                Element topLevelElement = mock(Element.class);
-                Element navElement = mock(Element.class);
-                Element navElement2 = mock(Element.class);
-                Elements elements = new Elements();
-                Attributes attributes = mock(Attributes.class);
-                Attributes attributes2 = mock(Attributes.class);
-                elements.add(0, navElement);
-                elements.add(1, navElement2);
+        Document document = mock(Document.class);
+        Element topLevelElement = mock(Element.class);
+        Element navElement = mock(Element.class);
+        Element navElement2 = mock(Element.class);
+        Elements elements = new Elements();
+        Attributes attributes = mock(Attributes.class);
+        Attributes attributes2 = mock(Attributes.class);
+        elements.add(0, navElement);
+        elements.add(1, navElement2);
 
-                when(document.body()).thenReturn(topLevelElement);
-                when(topLevelElement.select(anyString())).thenReturn(elements);
-                when(attributes.get("href")).thenReturn("http://wiprodigital.com/what-we-do?value=work-three-circles-row");
-                when(attributes2.get("href")).thenReturn("http://wiprodigital.com/what-we-do");
-                when(navElement.attributes()).thenReturn(attributes);
-                when(navElement2.attributes()).thenReturn(attributes2);
+        when(document.body()).thenReturn(topLevelElement);
+        when(topLevelElement.select(anyString())).thenReturn(elements);
+        when(attributes.get("href")).thenReturn("http://wiprodigital.com/what-we-do?value=work-three-circles-row");
+        when(attributes2.get("href")).thenReturn("http://wiprodigital.com/what-we-do");
+        when(navElement.attributes()).thenReturn(attributes);
+        when(navElement2.attributes()).thenReturn(attributes2);
 
-                return document;
-            }
-        }
-        TestCrawler crawler = new TestCrawler();
-        List<Page> pages = crawler.connect(WIPRO_HOMEPAGE);
-        assertEquals(1, pages.get(0).getInternalUrls().size());
+        assertEquals(1, new PageParser().buildFromDocument(document, HOST_URL).getInternalUrls().size());
     }
 
 }
